f9fafb85e0b2fbe548b24060355d92526b328316
BennyPenny1 <bennyraines08@gmail.com>
2025-03-31 20:29:02 -0700

    removed a bunch

--- build/Desktop_Qt_6_8_3_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_calc_app_FILE C:/Users/benny/Repos/QT-personal/build/Desktop_Qt_6_8_3_MinGW_64_bit-Debug/calc_app.exe)
-set(__QT_DEPLOY_TARGET_calc_app_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_calc_app_RUNTIME_DLLS C:/Qt/6.8.3/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.8.3/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.8.3/mingw_64/bin/Qt6Core.dll)
